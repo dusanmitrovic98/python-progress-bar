674cfe85b395337fc7a0d3641f3ad577c29ad43a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-13 22:42:44 +0200

    Update progress-bar.py

--- progress-bar.py
+++ progress-bar.py
@@ -4,3 +4,4 @@ def progress_bar(label, total, progress):
     bar_length = 30
     filled_length = int(bar_length * progress / total)
     bar = '#' * filled_length + '-' * (bar_length - filled_length)
+    print(f'{label} [{bar}] {progress}/{total}', end='\r')
